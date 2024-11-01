e7dcb6fa47ff892ce57ed98d0b24755537235ad4
amirulganteng <153421615+Amrlmlna@users.noreply.github.com>
2024-11-01 16:27:51 +0800

    Add files via upload

---
+++ Searching.py
@@ -0,0 +1,22 @@
+import random
+
+while True:
+    NumRandom = random.choices(range(1, 11), k=20)
+    print("List angka acak:", NumRandom)
+
+    NumOption = int(input("Pilih Angka antara 1 s/d 10 : "))
+
+    IndexNumList = [index for index, value in enumerate(NumRandom) if value == NumOption]
+
+    if IndexNumList:
+        if NumOption % 2 == 0:
+            print(f"Y1 : angka yang anda pilih berada pada index {IndexNumList} dan merupakan bilangan genap")
+        else:
+            print(f"Y1 : angka yang anda pilih berada pada index {IndexNumList}")
+    else:
+        print("angka yang anda pilih tidak ada dalam list atau tidak genap")
+
+    retry = input("Apakah ingin mencoba program ini lagi? (y/n): ").strip().lower()
+    if retry != 'y':
+        print("Program selesai. Terima kasih banyak!")
+        break
